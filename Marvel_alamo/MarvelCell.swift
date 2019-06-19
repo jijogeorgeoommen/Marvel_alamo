cbef6fcf67d69157200c8d2718dc07fb060a27c3
jijogeorgeoommen <jijogeorgeoommen@gmail.com>
2019-06-19 13:36:23 +0530

    updated

---
+++ Marvel_alamo/MarvelCell.swift
@@ -0,0 +1,26 @@
+//
+//  MarvelCell.swift
+//  Marvel_alamo
+//
+//  Created by JIJO G OOMMEN on 19/06/19.
+//  Copyright © 2019 JIJO G OOMMEN. All rights reserved.
+//
+
+import UIKit
+
+class MarvelCell: UITableViewCell {
+
+    @IBOutlet weak var teamoutlet: UILabel!
+    @IBOutlet weak var nameoutlet: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Marvel_alamo/ViewController.swift
+++ Marvel_alamo/ViewController.swift
@@ -7,12 +7,72 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class Hero {
+    var name : String?
+    var team : String?
 
+    init(name : String?,team :String?) {
+        
+        self.name = name
+        self.team = team
+        
+    }
+    
+    
+}
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var heroes = [Hero]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return heroes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = marveltableview.dequeueReusableCell(withIdentifier: "MarvelCellid")as! MarvelCell
+        
+      // let hero : Hero
+       //hero = heroes[indexPath.row]
+        
+        cell.nameoutlet.text =  heroes[indexPath.row].name as! String
+        cell.teamoutlet.text = heroes[indexPath.row].team as! String
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 200
+    }
+    
+
+    @IBOutlet weak var marveltableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Alamofire.request("https://simplifiedcoding.net/demos/marvel/").responseJSON {respons in
+            
+            if let json = respons.result.value {
+                
+                let heroesArray : NSArray  = json as! NSArray
+                
+               // print(heroesArray)
+                
+                for i in 0..<heroesArray.count{
+                    
+                    self.heroes.append(Hero( name: (heroesArray[i] as AnyObject).value(forKey: "name") as? String,
+                        team: (heroesArray[i] as AnyObject).value(forKey: "team") as? String))
+                    
+                 print(self.heroes)
+                    
+                }
+                
+                self.marveltableview.reloadData()
+            }
+        }
     }
 
 
